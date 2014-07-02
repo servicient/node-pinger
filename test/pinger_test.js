2219c534ead253e2d938de10320bf6ff1b96c738
Luke Wendling <luke@lukewendling.com>
2014-07-02 09:57:20 -0500

    add first pinger module test

---
+++ test/pinger_test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var expect = require('chai').expect;
+var pinger = require('../lib/pinger');
+
+describe('pinger', function() {
+  describe('api', function() {
+    expect(pinger({interval: 3})).to.respondTo('start');
+  });
+});
